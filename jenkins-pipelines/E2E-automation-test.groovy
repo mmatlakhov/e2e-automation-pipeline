a3f0a2a930d3fb3c847c2f0745a176d3a193af8a
mmatlakhov <mmatlakhov@pagantis.com>
2020-02-21 16:52:14 +0100

    add maven image

--- jenkins-pipelines/E2E-automation-test.groovy
+++ jenkins-pipelines/E2E-automation-test.groovy
@@ -1,35 +1,44 @@
-#!groovy
-node {
-    stage('Git checkout') { // for display purposes
-        git 'https://github.com/mmatlakhov/e2e-automation-pipeline.git'
-    }
-    stage('Smoke') {
-        try {
-            sh "mvn clean verify -Dtags='type:Smoke'"
-        } catch (err) {
 
-        } finally {
-            publishHTML (target: [
-                    reportDir: 'target/site/serenity',
-                    reportFiles: 'index.html',
-                    reportName: "Smoke tests report"
-            ])
+pipeline {
+    agent {
+        docker {
+            image 'maven:3-alpine'
+            args '-v /root/.m2:/root/.m2'
         }
     }
-    stage('API') {
-        try {
-            sh "mvn clean verify -Dtags='type:API'"
-        } catch (err) {
+    stages {
 
-        } finally {
-            publishHTML (target: [
-                    reportDir: 'target/site/serenity',
-                    reportFiles: 'index.html',
-                    reportName: "API tests report"
-            ])
+        stage('Git checkout') { // for display purposes
+            git 'https://github.com/mmatlakhov/e2e-automation-pipeline.git'
         }
-    }
-    stage('Results') {
-        junit '**/target/failsafe-reports/*.xml'
+        stage('Smoke') {
+            try {
+                sh "mvn clean verify -Dtags='type:Smoke'"
+            } catch (err) {
+
+            } finally {
+                publishHTML (target: [
+                        reportDir: 'target/site/serenity',
+                        reportFiles: 'index.html',
+                        reportName: "Smoke tests report"
+                ])
+            }
+        }
+        stage('API') {
+            try {
+                sh "mvn clean verify -Dtags='type:API'"
+            } catch (err) {
+
+            } finally {
+                publishHTML (target: [
+                        reportDir: 'target/site/serenity',
+                        reportFiles: 'index.html',
+                        reportName: "API tests report"
+                ])
+            }
+        }
+        stage('Results') {
+            junit '**/target/failsafe-reports/*.xml'
+        }
     }
 }
